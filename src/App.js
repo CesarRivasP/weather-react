cc2b8541f80603f53dff498d1802908ea96f705b
CesarRivasP <cesarjose58@gmail.com>
2019-06-08 17:44:41 -0400

    implemented form component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import Header from './components/header';
+import Form from './components/form';
 
 
 function App() {
   return (
     <div className="App">
-      <Header title="Clima" />
+      <Header title="Aplicación del Clima" />
+
+      <div className="container-form">
+        <div className="container">
+          <div className="row">
+            <div className="col s12 m6">
+              <Form />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
