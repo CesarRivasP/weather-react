576cf3fd94c7b776d4cbdc32f682136e519c3ef3
CesarRivasP <cesarjose58@gmail.com>
2019-06-08 18:10:13 -0400

    Added data of the state and added conditions

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/header';
 import Form from './components/form';
 
 
 function App() {
+
+  // state principal
+  const [city, setCity] = useState('');
+
+  const [country, setCountry] = useState('')
+
+  const consultData = (data) => {
+    console.log(data);
+
+    if(data.city === '' || data.country === ''){
+      return;
+    }
+
+    // si existen ambos, agregarlos al state
+    setCity(city: data.city);
+    setCountry(country: data.country);
+  }
+
   return (
     <div className="App">
       <Header title="Aplicación del Clima" />
@@ -12,7 +30,7 @@ function App() {
         <div className="container">
           <div className="row">
             <div className="col s12 m6">
-              <Form />
+              <Form consultData={consultData} />
             </div>
           </div>
         </div>
